6229f0c32cf3d2ea34ebe1e01015d5d2c09d9a4a
cracker <crackerwork@126.com>
2023-06-15 17:17:42 +0800

    refactor:message移动到工作空间域

---
+++ src/Mi.Admin/Areas/Workspace/Controllers/MessageController.cs
@@ -0,0 +1,35 @@
+﻿using Mi.Core.Attributes;
+using Mi.Core.Models;
+using Mi.Core.Models.Paging;
+using Mi.Entity.System;
+using Mi.IService.System;
+using Mi.IService.System.Models;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mi.Admin.Areas.Workspace.Controllers
+{
+    [Area("Workspace")]
+    [Authorize]
+    public class MessageController : Controller
+    {
+        private readonly IMessageService _messageService;
+        public MessageController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        [AuthorizeCode("Workspace:Message:Page")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost, AuthorizeCode("Workspace:Message:Readed")]
+        public async Task<MessageModel> Readed(IList<long> ids) => await _messageService.ReadedAsync(ids);
+
+        [HttpPost, AuthorizeCode("Workspace:Message:Query")]
+        public async Task<MessageModel<PagingModel<SysMessage>>> GetMessageList([FromBody] MessageSearch search) => await _messageService.GetMessageListAsync(search);
+    }
+}
